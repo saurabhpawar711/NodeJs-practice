e29fd42f5371d54c0427ae50e511e342525ec45a
saurabhpawar711 <saurabhpawar1107@gmail.com>
2023-06-13 17:34:15 +0530

    Lets Revise Javascript concepts

--- Practice.js
+++ Practice.js
@@ -1,2 +1,17 @@
-const fs = require('fs')
-fs.writeFileSync('text.txt', 'Hello World');
+// const fs = require('fs')
+// fs.writeFileSync('text.txt', 'Hello World');
+
+let productOfTwoNums = (a,b) => a*b
+
+console.log(productOfTwoNums(2,3));
+
+
+const student = {
+    name : 'saurabh',
+    age : 22,
+    greet : function() {
+        console.log('Hello from', this.name)
+    } 
+}
+
+student.greet()
