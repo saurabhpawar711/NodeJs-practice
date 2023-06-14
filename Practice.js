264fd5060109737281ec671b79a2b7bdea5b4665
saurabhpawar711 <saurabhpawar1107@gmail.com>
2023-06-14 20:17:42 +0530

    Clean Up Code

--- Practice.js
+++ Practice.js
@@ -56,63 +56,7 @@
 // print();
 
 const http = require('http')
-const app = http.createServer((req, res) => {
-
-    const url = req.url;
-    const method = req.method;
-    const fs = require('fs')
-    const path = require('path')
-
-
-    //     if(url === '/'){
-    //         res.end('<h1>Welccome to home page</h1>')
-    //     }
-    //     else if(url === '/node'){
-    //         res.end('<h1>Welcome to my Node Js project</h1>')
-    //     }
-    //     else if(url === '/about')
-    //     {
-    //         res.end('<h1>Welcome to about Us page</h1>')
-    //     }
-
-    // })
-
-    if (url === '/') {
-        fs.readFile(path.join(__dirname, 'message.txt'), (err, response) => {
-            if (err) {
-                throw err;
-            }
-            const msg = Buffer.from(response).toString();
-            res.write('<html>')
-            res.write('<head><title>Node JS</title></head>')
-            res.write('<body>' + msg + '</body>')
-            res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>')
-            res.write('</html>')
-            return res.end();
-        })
-    }
-    else if (url === '/message' && method === 'POST') {
-        const body = [];
-        req.on('data', (chunk) => {
-            body.push(chunk);
-        })
-        req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            var message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt', message)
-        })
-
-        res.statusCode = 302;
-        res.setHeader('location', '/');
-        return res.end();
-    } else {
-        res.setHeader('Content-Type', 'text/html')
-        res.write('<html>')
-        res.write('<head><title>Node JS</title></head>')
-        res.write('<body><h1>Welcome to Node JS</h1></body>')
-        res.write('</html>')
-        res.end();
-    }
-})
-
+const requestHandler = require('./routes')
+const app = http.createServer(requestHandler)
+// console.log(routes.text);
 app.listen(4000)
