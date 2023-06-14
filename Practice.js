f3c01bf243b973de7c9c6b59f3cc56e494790d97
saurabhpawar711 <saurabhpawar1107@gmail.com>
2023-06-14 10:09:13 +0530

    created Server and name is printed.

--- Practice.js
+++ Practice.js
@@ -1,17 +1,63 @@
 // const fs = require('fs')
 // fs.writeFileSync('text.txt', 'Hello World');
 
-let productOfTwoNums = (a,b) => a*b
+// let productOfTwoNums = (a,b) => a*b
 
-console.log(productOfTwoNums(2,3));
+// console.log(productOfTwoNums(2,3));
 
 
-const student = {
-    name : 'saurabh',
-    age : 22,
-    greet : function() {
-        console.log('Hello from', this.name)
-    } 
-}
+// const student = {
+//     name : 'saurabh',
+//     age : 22,
+//     greet : function() {
+//         console.log('Hello from', this.name)
+//     } 
+// }
 
-student.greet()
+// student.greet()
+
+// const fruits = ['apple', 'oranges', ' ', 'mango', ' ', 'lemon'];
+
+// const addElement = 'empty string'
+
+// for (let i = 0; i < fruits.length; i++) {
+//     if (fruits[i] === ' ') {
+//         fruits[i] = addElement;
+//     }
+// }
+// console.log(fruits)
+// console.log('a');
+
+// console.log('b');
+
+// function printc() {
+//     return new Promise((res) => {
+//         setTimeout(() => {
+//             res(console.log('c')), 3000})
+//     })
+// }
+
+// function printd() {
+//     return new Promise((res) => {
+//         setTimeout(() => {
+//             res(console.log('d')), 0})
+//     })
+// }
+//  function printe() {
+//     console.log('e');
+//  }
+
+// async function print() {
+//     await printc();
+//     await printd();
+//     printe();
+// }
+
+// print();
+
+const http = require('http')
+const app = http.createServer((req, res) => {
+    res.end('Saurabh')
+})
+
+app.listen(4000)
