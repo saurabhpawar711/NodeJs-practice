a196418d57f3786842d78f671814e288cd2420ca
saurabhpawar711 <saurabhpawar71100@gmail.com>
2024-01-10 21:08:55 +0530

    used React Refs

--- practice-project/src/components/Form.js
+++ practice-project/src/components/Form.js
@@ -1,27 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 import Modal from "./Modal";
 import "./Form.css";
 
 const Form = (props) => {
-  const [username, setUsername] = useState("");
-  const [age, setAge] = useState("");
+  // const [username, setUsername] = useState("");
+  // const [age, setAge] = useState("");
   const [modal, setModal] = useState(false);
   const [modalError, setModalError] = useState("");
 
-  const usernameHandler = (event) => {
-    setUsername(event.target.value);
-  };
+  const usernameRef = useRef();
+  const ageRef = useRef();
+  const collegeRef = useRef();
 
-  const ageHandler = (event) => {
-    setAge(event.target.value);
-  };
+  // const usernameHandler = (event) => {
+  //   setUsername(event.target.value);
+  // };
+
+  // const ageHandler = (event) => {
+  //   setAge(event.target.value);
+  // };
 
   const getUserDetails = (event) => {
     event.preventDefault();
-    if (username.trim() === "" || age.trim() === "") {
+    const username = usernameRef.current.value;
+    const age = ageRef.current.value;
+    const college = collegeRef.current.value;
+    if (username.trim() === "" || age.trim() === "" || college.trim() === "") {
       setModal(true);
-      setModalError("Please enter valid name and age (non-empty values)");
+      setModalError("Please enter valid inputs (non-empty values)");
       return;
     } else if (Number(age) <= 0) {
       setModal(true);
@@ -31,10 +38,14 @@ const Form = (props) => {
     const userData = {
       username,
       age,
+      college,
     };
     props.userData(userData);
-    setUsername("");
-    setAge("");
+    usernameRef.current.value = "";
+    ageRef.current.value = "";
+    collegeRef.current.value = "";
+    // setUsername("");
+    // setAge("");
   };
 
   const closeModal = () => {
@@ -47,17 +58,21 @@ const Form = (props) => {
         <label>Username</label>
         <input
           type="text"
-          value={username}
-          onChange={usernameHandler}
+          // value={username}
+          // onChange={usernameHandler}
+          ref={usernameRef}
           className="input-field"
         />
         <label>Age(Years)</label>
         <input
           type="number"
           className="input-field"
-          value={age}
-          onChange={ageHandler}
+          // value={age}
+          // onChange={ageHandler}
+          ref={ageRef}
         />
+        <label>College Name</label>
+        <input type="text" className="input-field" ref={collegeRef} />
         <button className="add-user-btn">Add User</button>
       </form>
     </>
--- practice-project/src/components/UserList.js
+++ practice-project/src/components/UserList.js
@@ -5,13 +5,13 @@ const UserList = (props) => {
   const userData = props.userData;
   return (
     <>
-      {userData.length === 0 && <h2 style={{color: "#fff"}}>No users!!!</h2>}
+      {userData.length === 0 && <h2 style={{ color: "#fff" }}>No users!!!</h2>}
       {userData.length > 0 && (
         <div className="userlist">
           <ul>
             {userData.map((user) => (
               <li className="username" key={user.id}>
-                {user.username} ( {user.age} years old )
+                {user.username} ( {user.age} years old ) | College: {user.college}
               </li>
             ))}
           </ul>
