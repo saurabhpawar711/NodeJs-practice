ba9379a44f553398c8c3b5b80c8a8bd7ccb6667c
saurabhpawar711 <saurabhpawar71100@gmail.com>
2024-01-10 18:16:00 +0530

    used React portals for modal

--- practice-project/src/components/Modal.js
+++ practice-project/src/components/Modal.js
@@ -1,21 +1,36 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
 import "./Modal.css";
+
+const ModalOverlay = (props) => {
+  return (
+    <div className="modal-overlay">
+      <div className="modal">
+        <h2>Invalid Input</h2>
+        <p>{props.errorMessage}</p>
+        <button onClick={props.onConfirm} className="modal-btn">
+          Okay
+        </button>
+      </div>
+    </div>
+  );
+};
 
 const Modal = (props) => {
   const closeModal = () => {
     props.closeModal();
   };
   return (
-    <div className="modal-overlay">
-      <div className="modal">
-        <h2>Invalid Input</h2>
-        <p>{props.errorMessage}</p>
-        <button onClick={closeModal} className="modal-btn">
-          Okay
-        </button>
-      </div>
-    </div>
+    <React.Fragment>
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          errorMessage={props.errorMessage}
+          onConfirm={closeModal}
+        />,
+        document.getElementById("modal")
+      )}
+    </React.Fragment>
   );
 };
 
